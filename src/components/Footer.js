154ce074a2fa5fd6a9972d459fb0535ec16e4514
frankcollins3 <fwc3rd@gmail.com>
2020-12-18 12:17:29 -0500

    [mern-auth-client] add about and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer bd-dark">
+            <div className="container text-center">
+                <span className="text-muted">@2020 Built With Love</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
